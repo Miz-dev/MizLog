1ce88c227341e7a4f7b219c9c9d8b55957b20874
shn <topdog.shn.01@gmail.com>
2022-05-05 22:44:06 +0900

    🐛 Fix a bug

--- src/layouts/AppLayout/index.tsx
+++ src/layouts/AppLayout/index.tsx
@@ -2,7 +2,7 @@ import { Footer } from "src/layouts/AppLayout/Footer";
 import { Header } from "src/layouts/AppLayout/Header";
 import { Side } from "src/layouts/AppLayout/Side";
 
-export const AppLayout = (props) => {
+export const AppLayout = (props: any) => {
 	return (
 		<div className="mx-auto flex min-h-screen max-w-7xl flex-col items-center px-2">
 			<Header />
