3f70be95b278547880705bfa0a93b8d28d1068ea
shn <topdog.shn.01@gmail.com>
2022-05-06 08:13:16 +0900

    📝 comment out

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,20 +16,6 @@ type Props = MicroCMSListResponse<Blog>;
 
 const Home: NextPage<Props> = (props) => {
 	return (
-		// <div>
-		// 	<p>{`記事の総数： ${props.totalCount}件`}</p>
-		// 	<ul>
-		// 		{props.contents.map((content) => {
-		// 			return (
-		// 				<li key={content.id}>
-		// 					<Link href={`/blog/${content.id}`}>
-		// 						<a>{content.title}</a>
-		// 					</Link>
-		// 				</li>
-		// 			);
-		// 		})}
-		// 	</ul>
-		// </div>
 		<div className="w-full">
 			<Head>
 				<title>MizLog | Web制作の備忘録</title>
@@ -58,7 +44,7 @@ const Home: NextPage<Props> = (props) => {
 						})}
 					</ul>
 				</Tabs.Tab>
-				<Tabs.Tab label="Life Hack" className="w-40">
+				{/* <Tabs.Tab label="Life Hack" className="w-40">
 					<ul className="mt-8 grid grid-cols-2 gap-6 md:grid-cols-2 md:gap-8">
 						{props.contents.map((content) => {
 							return (
@@ -80,7 +66,7 @@ const Home: NextPage<Props> = (props) => {
 							);
 						})}
 					</ul>
-				</Tabs.Tab>
+				</Tabs.Tab> */}
 			</Tabs>
 		</div>
 	);
