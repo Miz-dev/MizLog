c8525242df119cb6c5def514e7667a15c76e0ef8
shn <topdog.shn.01@gmail.com>
2022-05-06 09:11:44 +0900

    📝 Add post

--- src/pages/zenn/index.tsx
+++ src/pages/zenn/index.tsx
@@ -90,6 +90,24 @@ const Zenn = () => {
 				<Tabs.Tab label="Ideas" className="w-40">
 					<ul className="mt-8 grid grid-cols-2 gap-6 md:grid-cols-2 md:gap-8">
 						<li className="rounded-lg shadow">
+							<Link href="https://zenn.dev/miz_dev/articles/f0b926126c138f">
+								<a
+									target="_blank"
+									className="flex h-full flex-col justify-between px-6 py-10 hover:bg-gray-100"
+								>
+									<div className="mb-5 flex truncate text-sm">
+										<div className="mr-1 h-4 w-4">
+											<Image src={zennLogo} alt="zenn-logo" />
+										</div>
+										Zenn.dev
+									</div>
+									<h2 className="truncate text-lg font-bold text-blue-900">
+										ポモドーロ・テクニックとは？【集中して作業したい方必見！】
+									</h2>
+								</a>
+							</Link>
+						</li>
+						<li className="rounded-lg shadow">
 							<Link href="https://zenn.dev/miz_dev/articles/a37c62460f21ea">
 								<a
 									target="_blank"
