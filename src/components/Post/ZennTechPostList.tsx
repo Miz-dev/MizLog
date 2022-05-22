e684dc232447b21a79b4d0e17d75d165abfb25c7
shn <topdog.shn.01@gmail.com>
2022-05-22 22:44:43 +0900

    📝 Add post

--- src/components/Post/ZennTechPostList.tsx
+++ src/components/Post/ZennTechPostList.tsx
@@ -6,6 +6,24 @@ export const ZennTechPostList = () => {
 	return (
 		<ul className="mt-6 grid grid-cols-1 gap-4 xs:grid-cols-2 xs:gap-6 md:mt-8 md:gap-8">
 			<li className="rounded-lg shadow">
+				<Link href="https://zenn.dev/miz_dev/articles/4e6baa5b747c5d">
+					<a
+						target="_blank"
+						className="flex h-full flex-col justify-between px-6 py-10 hover:bg-gray-100"
+					>
+						<div className="mb-5 flex truncate text-sm">
+							<div className="mr-1 h-4 w-4">
+								<Image src={zennLogo} alt="zenn-logo" />
+							</div>
+							Zenn.dev
+						</div>
+						<h2 className="truncate text-lg font-bold text-blue-900">
+							Reactの基本的なところを備忘として残しておく
+						</h2>
+					</a>
+				</Link>
+			</li>
+			<li className="rounded-lg shadow">
 				<Link href="https://zenn.dev/miz_dev/articles/c26ab943f35b8f">
 					<a
 						target="_blank"
