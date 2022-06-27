b0d19765605ada5a54d1d01b8b74c2f23e7ebd43
shn <topdog.shn.01@gmail.com>
2022-06-27 22:14:24 +0900

    ✨ Add Google Analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,25 @@
 import "src/lib/tailwind.css";
 import type { AppProps } from "next/app";
 import { AppLayout } from "src/layout/AppLayout";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import { GA_TRACKING_ID, pageview } from "src/lib/gtag";
 
-function MyApp({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps }: AppProps): JSX.Element {
+	const router = useRouter();
+	useEffect(() => {
+		// GA_TRACKING_ID が設定されていない場合は、処理終了
+		if (!GA_TRACKING_ID) return;
+
+		const handleRouteChange = (url: string) => {
+			pageview(url);
+		};
+		router.events.on("routeChangeComplete", handleRouteChange);
+		return () => {
+			router.events.off("routeChangeComplete", handleRouteChange);
+		};
+	}, [router.events]);
+
 	return (
 		<AppLayout>
 			<Component {...pageProps} />
@@ -10,4 +27,12 @@ function MyApp({ Component, pageProps }: AppProps) {
 	);
 }
 
-export default MyApp;
+// function MyApp({ Component, pageProps }: AppProps) {
+// 	return (
+// 		<AppLayout>
+// 			<Component {...pageProps} />
+// 		</AppLayout>
+// 	);
+// }
+
+// export default MyApp;
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,5 @@
 import NextDocument, { Html, Head, Main, NextScript } from "next/document";
+import { GA_TRACKING_ID } from "src/lib/gtag";
 
 export default class Document extends NextDocument {
 	render() {
@@ -30,6 +31,26 @@ export default class Document extends NextDocument {
 					/>
 					<meta name="msapplication-TileColor" content="#ffffff" />
 					<meta name="theme-color" content="#ffffff" />
+					{GA_TRACKING_ID && (
+						<>
+							<script
+								async
+								src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+							/>
+							<script
+								dangerouslySetInnerHTML={{
+									__html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+        `,
+								}}
+							/>
+						</>
+					)}
 				</Head>
 				<body>
 					<Main />
