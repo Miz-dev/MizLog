5535cc0a0858f5c4f27bdd4c6f1329c7da009ec1
shn <topdog.shn.01@gmail.com>
2022-07-04 22:48:10 +0900

    📈 Update Analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,38 +1,22 @@
 import "src/lib/tailwind.css";
 import type { AppProps } from "next/app";
 import { AppLayout } from "src/layout/AppLayout";
-import { useEffect } from "react";
 import { useRouter } from "next/router";
-import { GA_TRACKING_ID, pageview } from "src/lib/gtag";
+import { GoogleAnalytics, usePageView } from "../lib/gtag";
 
-export default function App({ Component, pageProps }: AppProps): JSX.Element {
+const App = ({ Component, pageProps }: AppProps): JSX.Element => {
 	const router = useRouter();
-	useEffect(() => {
-		// GA_TRACKING_ID が設定されていない場合は、処理終了
-		if (!GA_TRACKING_ID) return;
 
-		const handleRouteChange = (url: string) => {
-			pageview(url);
-		};
-		router.events.on("routeChangeComplete", handleRouteChange);
-		return () => {
-			router.events.off("routeChangeComplete", handleRouteChange);
-		};
-	}, [router.events]);
+	usePageView();
 
 	return (
-		<AppLayout>
-			<Component {...pageProps} />
-		</AppLayout>
+		<>
+			<GoogleAnalytics />
+			<AppLayout>
+				<Component {...pageProps} />
+			</AppLayout>
+		</>
 	);
-}
+};
 
-// function MyApp({ Component, pageProps }: AppProps) {
-// 	return (
-// 		<AppLayout>
-// 			<Component {...pageProps} />
-// 		</AppLayout>
-// 	);
-// }
-
-// export default MyApp;
+export default App;
