1df9a5906d531d573ec9868b9147e7c28ab34925
Miz-dev <mizlog.dev@gmail.com>
2024-05-04 09:42:50 +0900

    Footer：画面下固定の記述変更

--- src/layout/AppLayout/AppLayout.tsx
+++ src/layout/AppLayout/AppLayout.tsx
@@ -4,7 +4,7 @@ import { Side } from "src/layout/AppLayout/Side/Side";
 
 export const AppLayout = (props: any) => {
   return (
-    <div className="mx-auto grid min-h-screen w-full max-w-7xl grid-cols-[100%] grid-rows-[auto_1fr_auto] px-5">
+    <div className="mx-auto  min-h-screen w-full max-w-7xl px-5">
       <Header />
       <div className="flex w-full flex-col gap-10 md:flex-row md:justify-between">
         {props.children}
--- src/layout/AppLayout/Footer/Footer.tsx
+++ src/layout/AppLayout/Footer/Footer.tsx
@@ -1,7 +1,7 @@
 // import Link from "next/link";
 export const Footer = () => {
   return (
-    <footer className="mt-10 flex w-full justify-center border-t py-4">
+    <footer className="sticky top-[100vh] flex w-full justify-center border-t py-4">
       <small>
         &copy;{`${new Date().getFullYear()} MizLog. All rights reserved.`}
       </small>
